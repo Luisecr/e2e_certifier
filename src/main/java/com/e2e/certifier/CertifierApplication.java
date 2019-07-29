d8f550e667b1e13cb3d222c9e47e6ed5b07c252f
Cervantes Roldan Luis Enrique <MB84608@cb.bbvabancomer.com.mx>
2019-07-29 16:35:13 -0500

    first commit

---
+++ src/main/java/com/e2e/certifier/CertifierApplication.java
@@ -0,0 +1,13 @@
+package com.e2e.certifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifierApplication.class, args);
+	}
+
+}
